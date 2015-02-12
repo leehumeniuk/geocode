70f22eae14c3c77a0eb244031246f23f1222ce35
Lee <lee.humeniuk@gmail.com>
2015-02-12 11:23:57 -0700

    WORKING! Only taking top 1000 entries from DB

--- PostalCode/PostalCode/Form1.cs
+++ PostalCode/PostalCode/Form1.cs
@@ -3,17 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
-using System.Reflection;
-using Noesis.Javascript;
 using System.IO;
 using System.Net;
-using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Linq;
 
 //geocoding api key: 
 //neighbourhood url example: https://maps.googleapis.com/maps/api/geocode/json?address=Winnetka&key=API_KEY
@@ -39,41 +38,19 @@ namespace PostalCode
 
         private void retrieveDataButton_Click(object sender, EventArgs e)
         {
-            getJson();  
-          
+            getData();
         }
 
-        //returns Json object
-        private void getJson()
-        {
 
-            String dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\JSON";
-            if (!Directory.Exists(dir))
-            {
-                DirectoryInfo di = Directory.CreateDirectory(dir);
-            }
 
-            //use forloop to grab address from file
-            //function getInput() returns string = address!, use counter to return ID number
-            String address = "Rutherford"; //temp string. actual string comes from DB
-            //change API key every 2500~ by mod 2500
-            String url = parseURL(address);
-            createOutput(url, dir);
-            messageLabel.Text = "Status: Succes!";
-            
-        }
-
-        
-        //takes in address or neighbourhood.
-        //returns url sent to API
         private String parseURL(String input)
         {
             String address = input;
             String city = ",+Edmonton,+AB,+CA";
             String locationInfo;
-            String url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+            String url = "https://maps.googleapis.com/maps/api/geocode/xml?address=";
             //need to build array of keys
-            String key = "";
+            String key = "&key=AIzaSyDvkrmvwL3RkHRNsqxP2IHHXjOqi5JU59g";
             StringBuilder builder = new StringBuilder(address);
             int length = address.Length;
 
@@ -101,41 +78,59 @@ namespace PostalCode
 
         }
 
-        private void createOutput(String url, String dir)
+        private void getData()
         {
-
-            //connecting to API and retrieving information////
+            ArrayList dataset = addressData();
             WebClient client = new WebClient();
-            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");//arbitrary header
-            Stream data = client.OpenRead(url);
-            StreamReader reader = new StreamReader(data);
-            String result = reader.ReadToEnd();
-            /////////////////////////////////////////////////
+            Stream data = null;
+            String url;
+            XDocument xml = new XDocument();
+            XNode node = null;
+            System.Text.RegularExpressions.Regex pattern = new System.Text.RegularExpressions.Regex(@"(?<=>)(.*)(?=<)");
+            System.Text.RegularExpressions.Match match = null;
+            String address = null;
+            String postal_code = null;
 
-            //JSON Serializer to pick specific elements//////
-            JsonSerializer serial = new JsonSerializer();
-            //serial.Serialize(textFile, result);
-            /////////////////////////////////////////////////
 
-            //writing JSON result to file///////
-            String filePath = dir + @"\" + System.DateTime.Now.Minute.ToString() + "_Json.txt";
-            StreamWriter textFile = new StreamWriter(filePath);
-            textFile.WriteLine(result);
-            textFile.Close();
-            ////////////////////////////////////
+            for (int i = 0; i < dataset.Count; i++)
+            {
+                //set url
+                url = parseURL(dataset[i].ToString());
+                address = dataset[i].ToString();
 
-            //writing results to database//////
-            //TODO
-            ///////////////////////////////////
+                //connecting to API and retrieving information////
 
-            //result test///
-            //Console.WriteLine(filePath); //displays contents of json object in console
-            ////////////////
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");//arbitrary header
+                data = client.OpenRead(url);
+                xml = XDocument.Load(data);
+                foreach (XElement xe in xml.Descendants("type").Where(x => x.Value.Equals("postal_code")))
+                {
+                    node = xe.PreviousNode.PreviousNode;
+                }
 
-            //closing streams/////
+                if (node != null)
+                {
+                    match = pattern.Match(node.ToString());
+
+
+                    if (match.Success)
+                    {
+                        postal_code = match.Value;
+
+
+                        //write to DB
+                        //Console.WriteLine(postal_code);
+                        insertData(postal_code, address);
+                    }
+                }
+
+            }
+
             data.Close();
-            reader.Close();
-            //////////////////////
+          
+
+
+            
         }
 
         private void messageLabel_Click(object sender, EventArgs e)
@@ -148,11 +143,70 @@ namespace PostalCode
             this.Close();
         }
 
-        private void getData()
+        private ArrayList addressData()
         {
             String connectionString = "Data Source=WIN-FSBHP3NVE9G;Initial Catalog=TRAFFIC_COLLISION;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand command;
+            String sql = "SELECT TOP 1000 Location FROM Combined_Data";
+            SqlDataReader dataReader;
+            ArrayList results = new ArrayList();
+            String value;
+
+                try
+                {
+                    conn.Open();
+                    command = new SqlCommand(sql, conn);
+                    dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        //MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
+                        value = dataReader.GetValue(0).ToString();
+                        results.Add(value);
+                        
+                    }
+                    dataReader.Close();
+                    command.Dispose();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Cannot open connection ! ");
+                }
+
+                return results;
 
         }
 
+
+        private void insertData(String postal_code, String address)
+        {
+            String connectionString = "Data Source=WIN-FSBHP3NVE9G;Initial Catalog=TRAFFIC_COLLISION;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+            String sql = "UPDATE Combined_Data SET Postal_Code = '"+postal_code+"' WHERE Location = '"+address+"'";
+            SqlCommand command = new SqlCommand(sql, conn);
+
+            try
+            {
+                command.Connection.Open();
+
+                command.ExecuteNonQuery();
+
+                command.Dispose();
+
+                command.Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection ! ");
+            }
+        }
+
+
     }
+
 }
+
+
+
+
